726b445a04652bb98c91314cb5356aab5c045549
horejs <michal.horejs@inqool.cz>
2025-02-13 20:33:07 +0100

    CORS handle, dev/prod setup, fetching data using API

---
+++ src/main/client/src/Env.ts
@@ -0,0 +1,10 @@
+// Env.ts
+
+const { VITE_API_BASE_URL, ...otherViteConfig } = import.meta.env;
+
+export const Env = {
+    API_BASE_URL: VITE_API_BASE_URL as string,
+    __vite__: otherViteConfig,
+};
+
+console.log(Env);
